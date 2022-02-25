f88da64a4d45054527669f10167df5c5e2545a1c
njiti <kamaukan77@gmail.com>
2022-02-25 12:07:19 -0930

    Created a DailyFragmentMeal.java

--- app/src/main/java/com/example/mamlaka_grill/ui/DailyMeal/DailyFragmentMeal.java
+++ app/src/main/java/com/example/mamlaka_grill/ui/DailyMeal/DailyFragmentMeal.java
@@ -9,19 +9,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.example.mamlaka_grill.databinding.FragmentDailMealyBinding;
+import com.example.mamlaka_grill.databinding.DailyFragmentBinding;
+
 
 public class DailyFragmentMeal extends Fragment {
 
-    private FragmentDailyMealBinding binding;
+    private com.example.mamlaka_grill.databinding.DailyFragmentBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentDailyMealBinding.inflate(inflater, container, false);
+        binding = DailyFragmentBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDailyMeal;
         return root;
     }
 
